65a1b16f69f31cefab7b099a40b7e4b9ef4a7448
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2026-02-25 03:31:23 -0700

    New Branch

---
+++ Unity/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * h + transform.forward * v;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
